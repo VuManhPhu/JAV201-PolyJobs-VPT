d4bfddeece8cc40de878fccf80f5f9f85c691b0d
VuManhPhu <phubeeswar@gmail.com>
2026-02-04 07:17:11 +0700

    Delete PolyJobs directory

--- PolyJobs/src/main/java/dao/ShareDAO.java
+++
@@ -1,5 +0,0 @@
-package dao;
-
-import entity.Share;
-
-public interface ShareDAO extends GenericDAO<Share, Long> {}
--- PolyJobs/src/main/java/dao/ShareDAOImpl.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import entity.Share;
-
-public class ShareDAOImpl extends AbstractDAO<Share, Long> implements ShareDAO {
-	public ShareDAOImpl() {
-		super(Share.class);
-	}
-}
